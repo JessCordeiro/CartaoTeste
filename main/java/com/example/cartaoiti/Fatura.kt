2dc013fbe30176f16d866c1f739be4fc9c26f976
JessCordeiro <67651751+JessCordeiro@users.noreply.github.com>
2021-10-04 15:41:55 -0300

    rascunho

---
+++ main/java/com/example/cartaoiti/Fatura.kt
@@ -0,0 +1,13 @@
+package com.example.cartaoiti
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class Fatura : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.card_fatura)
+
+    }
+}
